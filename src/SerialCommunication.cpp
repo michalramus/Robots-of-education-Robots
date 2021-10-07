37027f69c1992a1916bbd3c6ff9ba82621e06d1b
Michał Ramus <ramus.michal21@gmail.com>
2021-10-07 14:09:49 +0200

    add and create readValues method
    add and create method that read values from Serial in
    SerialCommunication class

--- src/SerialCommunication.cpp
+++ src/SerialCommunication.cpp
@@ -11,7 +11,23 @@ SerialCommunication::~SerialCommunication() //destructor
     //empty
 }
 
-String SerialCommunication::readValues()
+String SerialCommunication::readValues() //read values from Serial
 {
-    
+    bool messageReceived = false; //was message completely received
+    String message = "";          //message from Serial
+
+    while (messageReceived == false) //waiting for message
+    {
+        if (Serial.available() == true)
+        {
+            message += Serial.available();
+
+            if ((message[0] == 'x') && (message[sizeof(message) / sizeof(message[0]) - 1] == 'x')) //check if message transfer is completed
+            {
+                messageReceived == true;
+            }
+        }
+    }
+
+    return message;
 }

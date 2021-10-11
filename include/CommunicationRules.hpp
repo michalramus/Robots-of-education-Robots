571a473b1247cf9cd92895a8dc8e41ca0d9ef861
Michał Ramus <ramus.michal21@gmail.com>
2021-10-11 20:30:04 +0200

    feat: create CommSymbol::getID methods
    Create getID method that search symbol ID by symbol. Second getID method
    search symbol ID from specific ID.

--- include/CommunicationRules.hpp
+++ include/CommunicationRules.hpp
@@ -6,6 +6,8 @@ enum CommSymbolID //id of communication symbols
 {
     startEndMessage = 0, //symbol that is on the begin and end of message
 
+
+    symbolsTabSize, //ID is equal to symbols tab size. IT HAVE TO BE AT THE END OF ENUM
 };
 
 enum DevType //ID of device types
@@ -17,7 +19,9 @@ class CommSymbol //communication symbols
 {
 public:
     static String getSymbol(int ID); //get symbols by ID
+
     static int getID(String symbol); //get ID of symbol
+    static int getID(String symbol, int startID); //get ID of symbol Start search from specific ID
 
 private:
     const static String symbols[]; //table with symbols

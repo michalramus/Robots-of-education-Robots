aa68d45eb6ea0a068b53143698fa51bde091a0ce
Michał Ramus <ramus.michal21@gmail.com>
2021-11-01 14:58:29 +0100

    refactor: set sentMessage[char, int] as private

--- include/SerialCommunication.hpp
+++ include/SerialCommunication.hpp
@@ -11,10 +11,8 @@ public:
     static Message &readMessage(); //read values from Serial
     
     static void sendMessage(Message message); //send message
-    static void sendMessage(char *message, uint16_t messageLength); //send message
     static void sendMessageReceived(); //send message, that last message was received
     
 private:
-
-
+    static void sendMessage(char *message, uint16_t messageLength); //send message
 };

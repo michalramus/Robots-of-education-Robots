93a21950eb7c73ce5e4fd2039c4b84410cdd48cc
Michał Ramus <ramus.michal21@gmail.com>
2021-11-02 17:06:14 +0100

    ref: change unsigned int to int and rename method to setMessageByJson
    Change unsigned int to int in Device, SymbolsBase, Message and
    SerialCommunication classes
    
    Rename Message::setJsonMessage to setMessageByJson

--- include/Message/Message.hpp
+++ include/Message/Message.hpp
@@ -9,21 +9,22 @@ public:
     Message();
     ~Message(); //destructor
 
-    void setJsonMessage(char *message); //add convert Json message to Message object !!!METHOD DELETE MESSAGE VARIABLE!!!
+    void setMessageByJson(char *message); //add convert Json message to Message object !!!METHOD DELETE MESSAGE VARIABLE!!!
 
 private:
     void deserializeMessage(char *message, JsonDocument &doc); //deserialize message to JsonDocument
 
-    uint16_t getMessageTypeID(JsonDocument &doc); //get message type ID
+    int16_t getMessageTypeID(JsonDocument &doc); //get message type ID
 
     void setupConfigMsg(JsonDocument &doc); //setup message as config
 
-    uint16_t messageType = -1;
+    //variables
+    int16_t messageType = -1;
 
         //config
         uint16_t *devTypes = nullptr;
     //car
-    uint8_t numOfCar = -1;
+    int8_t numOfCar = -1;
 };
 
 
--- include/SerialCommunication.hpp
+++ include/SerialCommunication.hpp
@@ -11,7 +11,7 @@ public:
     static Message &readMessage(); //read values from Serial
     
     static void sendMessage(Message message); //send message
-    static void sendSpecifiedMessage(uint16_t type); //send message, that last message was received
+    static void sendSpecifiedMessage(int16_t type); //send message, that last message was received
 
 
 private:

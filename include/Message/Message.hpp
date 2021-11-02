579900ef90b3a8b5eb08b446e431be2347a84f23
Michał Ramus <ramus.michal21@gmail.com>
2021-11-02 17:21:27 +0100

    feat: create getCharMessageLength and define getCharMessage
    Create methods in Message class:
    
    getCharMessage: convert message object to char array and save char array
    length to variable
    
    getCharMessageLength: return charMessageLength variable

--- include/Message/Message.hpp
+++ include/Message/Message.hpp
@@ -10,7 +10,10 @@ public:
     ~Message(); //destructor
 
     void setMessageByJson(char *message); //add convert Json message to Message object !!!METHOD DELETE MESSAGE VARIABLE!!!
-
+    
+    char *getCharMessage(); //get message converted to char array
+    int16_t getCharMessageLength(); //get length of char array with message after serialization
+    
 private:
     void deserializeMessage(char *message, JsonDocument &doc); //deserialize message to JsonDocument
 
@@ -19,6 +22,7 @@ private:
     void setupConfigMsg(JsonDocument &doc); //setup message as config
 
     //variables
+    int16_t charMessageLength = -1; //variable that stores length of char array with message
     int16_t messageType = -1;
 
         //config

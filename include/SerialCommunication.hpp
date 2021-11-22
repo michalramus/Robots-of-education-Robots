808a89999151247ee83f7658fc99ece13c725335
Michał Ramus <ramus.michal21@gmail.com>
2021-11-22 14:40:06 +0100

    ref: use IMessage interface in SerialCommunication class
    Use IMessage interface instead of Message class

--- include/SerialCommunication.hpp
+++ include/SerialCommunication.hpp
@@ -1,4 +1,5 @@
 #include "Message/Message.hpp"
+#include "Interfaces/IMessage.hpp"
 
 #include <WString.h>
 
@@ -8,9 +9,9 @@ class SerialCommunication
 public:
     static void setBaudRate(uint16_t baudRate);
 
-    static Message &readMessage(); //read values from Serial
+    static IMessage readMessage(IMessage messageClass); //receive message from Serial
     
-    static void sendMessage(Message message); //send message
+    static void sendMessage(IMessage message); //send message
     static void sendSpecifiedMessage(int16_t type); //send message, that last message was received
 
 

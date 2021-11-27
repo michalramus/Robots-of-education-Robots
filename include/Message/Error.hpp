cde7baaa8405f5b2862c169510c67bc07c7e61a4
Michał Ramus <ramus.michal21@gmail.com>
2021-11-27 10:44:00 +0100

    ref: delete unnecessary spaces and format correctly code

--- include/Message/Error.hpp
+++ include/Message/Error.hpp
@@ -5,18 +5,17 @@
 class Error
 {
 public:
-    void setError(uint16_t errorType, char* errorMessage); //set error object
+    void setError(uint16_t errorType, char *errorMessage); //set error object
 
     //getters of error object
     uint16_t getErrorType();
-    char* getErrorMessage();
+    char *getErrorMessage();
 
     bool isConfigured(); //is error msg configured
 
 private:
     uint16_t errorType = -1;
-    char* errorMessage = nullptr;
+    char *errorMessage = nullptr;
 
     bool configured = false; //is error object configured
 };
-

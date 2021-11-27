82691c0664ebfead751ef9f31debca43448dbf63
Michał Ramus <ramus.michal21@gmail.com>
2021-11-27 18:25:18 +0100

    feat: Add overload of Error:setError method
    Add errorValue variable that stores data that caused error and overload
    of setError method that can set this variable

--- include/Message/Error.hpp
+++ include/Message/Error.hpp
@@ -6,16 +6,19 @@ class Error
 {
 public:
     void setError(uint16_t errorType, char *errorMessage); //set error object
+    void setError(uint16_t errorType, char *errorMessage, char *errorValue); //set error object
 
     //getters of error object
     uint16_t getErrorType();
     char *getErrorMessage();
+    char *getErrorValue();
 
     bool isConfigured(); //is error msg configured
 
 private:
     uint16_t errorType = -1;
-    char *errorMessage = nullptr;
+    char *errorMessage = nullptr; 
+    char *errorValue = nullptr; //data that caused error
 
     bool configured = false; //is error object configured
 };
